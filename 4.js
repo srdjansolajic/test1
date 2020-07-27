f0359bb682dfb43bbd03f40bca2abdef0b528df6
srdjansolajic <solajic.srdjan@gmail.com>
2020-07-27 12:08:37 +0200

    obrisane {} zagrade

--- 4.js
+++ 4.js
@@ -1,16 +1,11 @@
 // Cetvrti zadatak
-{
-    let cifra = 5;
-    let count = '';
-    let prviRed = '';
+let cifra = 5;
+let count = '';
+let prviRed = '';
 
-    for (let i = 1; i <= 9; i++) {
-        prviRed += (i + ' '.repeat(2));
-        count += (i * cifra + ' ');
-    }
-    console.log(prviRed);
-    console.log(count);
+for (let i = 1; i <= 9; i++) {
+    prviRed += (i + ' '.repeat(2));
+    count += (i * cifra + ' ');
 }
-
-
-
+console.log(prviRed);
+console.log(count);
